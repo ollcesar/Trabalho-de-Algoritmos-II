3826c0ddf31a709c4513b9b08b13f5b08c73f55e
César <juniocesar.php@gmail.com>
2025-08-15 22:25:10 -0300

    at6

--- lixeira/teste.c
+++ lixeira/teste.c
@@ -51,12 +51,18 @@ void limparBuffer() {
     while ((c = getchar()) != '\n' && c != EOF);
 }
 
+void pausar() {
+    printf("\nPressione Enter para continuar...");
+    limparBuffer();
+}
+
 void cadastrarProduto() {
     if (qtdProdutos >= capProdutos) {
         capProdutos = (capProdutos == 0) ? 1 : capProdutos * 2;
         produto *temp = realloc(listaProdutos, capProdutos * sizeof(produto));
         if (temp == NULL) {
             printf("ERRO: Memoria insuficiente!\n");
+            pausar();
             return;
         }
         listaProdutos = temp;
@@ -69,6 +75,7 @@ void cadastrarProduto() {
     for (int i = 0; i < qtdProdutos; i++) {
         if (listaProdutos[i].codigoProd == listaProdutos[qtdProdutos].codigoProd) {
             printf("ERRO: Codigo ja existe!\n");
+            pausar();
             return;
         }
     }
@@ -94,12 +101,14 @@ void cadastrarProduto() {
 
     qtdProdutos++;
     printf("Produto cadastrado com sucesso!\n");
+    pausar();
 }
 
 void listarProdutos() {
     printf("\n--- Lista de Produtos ---\n");
     if (qtdProdutos == 0) {
         printf("Nenhum produto cadastrado.\n");
+        pausar();
         return;
     }
 
@@ -112,32 +121,112 @@ void listarProdutos() {
         printf("Fornecedor: %s\n", listaProdutos[i].nomeFornecedor);
         printf("Telefone: %ld\n", listaProdutos[i].foneFornecedor);
     }
+    pausar();
+}
+
+void editarProduto() {
+    if (qtdProdutos == 0) {
+        printf("Nenhum produto cadastrado para editar.\n");
+        pausar();
+        return;
+    }
+
+    printf("\n--- Editar Produto ---\n");
+    printf("Digite o codigo do produto: ");
+    int codigo;
+    scanf("%d", &codigo);
+
+    int posicao = -1;
+    for (int i = 0; i < qtdProdutos; i++) {
+        if (listaProdutos[i].codigoProd == codigo) {
+            posicao = i;
+            break;
+        }
+    }
+
+    if (posicao == -1) {
+        printf("Produto nao encontrado!\n");
+        pausar();
+        return;
+    }
+
+    printf("\nEditando produto: %s\n", listaProdutos[posicao].nomeProd);
+    printf("Deixe em branco para manter o valor atual\n");
+
+    printf("Nome atual: %s\n", listaProdutos[posicao].nomeProd);
+    printf("Novo nome: ");
+    limparBuffer();
+    char novoNome[100];
+    fgets(novoNome, 100, stdin);
+    if (strlen(novoNome) > 1) {
+        novoNome[strcspn(novoNome, "\n")] = '\0';
+        strcpy(listaProdutos[posicao].nomeProd, novoNome);
+    }
+
+    printf("Preco atual: %.2f\n", listaProdutos[posicao].preco);
+    printf("Novo preco: ");
+    char entradaPreco[20];
+    fgets(entradaPreco, 20, stdin);
+    if (strlen(entradaPreco) > 1) {
+        listaProdutos[posicao].preco = atof(entradaPreco);
+    }
+
+    printf("Estoque atual: %d\n", listaProdutos[posicao].estoque);
+    printf("Novo estoque: ");
+    char entradaEstoque[20];
+    fgets(entradaEstoque, 20, stdin);
+    if (strlen(entradaEstoque) > 1) {
+        listaProdutos[posicao].estoque = atoi(entradaEstoque);
+    }
+
+    printf("Fornecedor atual: %s\n", listaProdutos[posicao].nomeFornecedor);
+    printf("Novo fornecedor: ");
+    char novoFornecedor[100];
+    fgets(novoFornecedor, 100, stdin);
+    if (strlen(novoFornecedor) > 1) {
+        novoFornecedor[strcspn(novoFornecedor, "\n")] = '\0';
+        strcpy(listaProdutos[posicao].nomeFornecedor, novoFornecedor);
+    }
+
+    printf("Telefone atual: %ld\n", listaProdutos[posicao].foneFornecedor);
+    printf("Novo telefone: ");
+    char entradaTelefone[20];
+    fgets(entradaTelefone, 20, stdin);
+    if (strlen(entradaTelefone) > 1) {
+        listaProdutos[posicao].foneFornecedor = atol(entradaTelefone);
+    }
+
+    printf("\nProduto atualizado com sucesso!\n");
+    pausar();
 }
 
 void excluirProduto() {
     if (qtdProdutos == 0) {
         printf("Nenhum produto para excluir.\n");
+        pausar();
         return;
     }
 
+    printf("\n--- Excluir Produto ---\n");
+    printf("Digite o codigo do produto: ");
     int codigo;
-    printf("Codigo do produto a excluir: ");
     scanf("%d", &codigo);
 
-    int pos = -1;
+    int posicao = -1;
     for (int i = 0; i < qtdProdutos; i++) {
         if (listaProdutos[i].codigoProd == codigo) {
-            pos = i;
+            posicao = i;
             break;
         }
     }
 
-    if (pos == -1) {
+    if (posicao == -1) {
         printf("Produto nao encontrado!\n");
+        pausar();
         return;
     }
 
-    for (int i = pos; i < qtdProdutos - 1; i++) {
+    for (int i = posicao; i < qtdProdutos - 1; i++) {
         listaProdutos[i] = listaProdutos[i + 1];
     }
 
@@ -152,6 +241,7 @@ void excluirProduto() {
     }
 
     printf("Produto excluido com sucesso!\n");
+    pausar();
 }
 
 void menuProdutos() {
@@ -160,19 +250,21 @@ void menuProdutos() {
         printf("\n=== MENU PRODUTOS ===\n");
         printf("1. Cadastrar produto\n");
         printf("2. Listar produtos\n");
-        printf("3. Excluir produto\n");
-        printf("4. Voltar\n");
+        printf("3. Editar produto\n");
+        printf("4. Excluir produto\n");
+        printf("5. Voltar\n");
         printf("Escolha: ");
         scanf("%d", &opcao);
 
         switch(opcao) {
             case 1: cadastrarProduto(); break;
             case 2: listarProdutos(); break;
-            case 3: excluirProduto(); break;
-            case 4: break;
+            case 3: editarProduto(); break;
+            case 4: excluirProduto(); break;
+            case 5: break;
             default: printf("Opcao invalida!\n");
         }
-    } while (opcao != 4);
+    } while (opcao != 5);
 }
 
 int main() {
@@ -189,9 +281,9 @@ int main() {
 
         switch(opcao) {
             case 1: menuProdutos(); break;
-            case 2: break;
-            case 3: printf("Encerrando sistema...\n"); break;
-            default: printf("Opcao invalida!\n");
+            case 2: printf("\nModulo de funcionarios em desenvolvimento...\n"); pausar(); break;
+            case 3: printf("\nEncerrando sistema...\n"); break;
+            default: printf("\nOpcao invalida!\n");
         }
     } while (opcao != 3);
 
